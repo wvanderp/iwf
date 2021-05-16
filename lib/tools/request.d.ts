2720d596afcaab67e9e953eefedefe1f7c2022a7
Wouter van der Plas <wouterv.dplas@gmail.com>
2021-05-16 18:13:25 +0200

    add a request feature

---
+++ lib/tools/request.d.ts
@@ -0,0 +1,7 @@
+import Item from '../Item';
+/**
+ * @param {string} qid the id of the entity
+ * @returns {Promise<Item>} the promise of a wikidata Item
+ */
+export default function request(qid: string): Promise<Item>;
+//# sourceMappingURL=request.d.ts.map
---
+++ src/tools/request.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import {WikidataResponse} from '@wmde/wikibase-datamodel-types';
+import Item from '../Item';
+
+/**
+ * @param {string} qid the id of the entity
+ * @returns {string} the url with qid
+ */
+function baseURL(qid: string): string {
+    return `https://www.wikidata.org/wiki/Special:EntityData/${qid}.json`;
+}
+
+/**
+ * @param {string} qid the id of the entity
+ * @returns {Promise<Item>} the promise of a wikidata Item
+ */
+export default async function request(qid: string): Promise<Item> {
+    const url = baseURL(qid);
+    const {data} = await axios.get<WikidataResponse>(url);
+
+    const statementData = Object.values(data.entities)[0];
+
+    return new Item(statementData);
+}
