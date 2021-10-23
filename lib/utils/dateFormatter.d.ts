676078d8eb05e71ae9d781aa096225f6ec11913c
Wouter van der Plas <wouterv.dplas@gmail.com>
2021-10-23 17:44:42 +0200

    removed generated files from repo

--- lib/utils/dateFormatter.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * formats a given date as a date that is compatible with wikidata
- *
- * @private
- * @param {Date} date the date that will be formatted
- * @returns {string} the formatted date
- */
-export default function dateFormatter(date: Date): string;
-//# sourceMappingURL=dateFormatter.d.ts.map
